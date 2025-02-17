118c470b40e6d11526deb760518c270cc0a32121
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-17 19:02:13 +0900

    added validations for user, booking and vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,10 @@
 class Vehicle < ApplicationRecord
+  has_many :bookings
+
+  validates :type, :brand, :model, :description, :location, :price_day, presence: true
+  enum type: { airplane: 'airplane', car: 'car', boat: 'boat', motorcycle: 'motorcycle' }
+  validates :type, inclusion: { in: types.keys }
+  validates :description, length: { in: 15..150 }
+
+
 end
