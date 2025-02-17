4eed2513fe8f4d48181ebf21e6c6f284c88157a7
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-17 19:12:53 +0900

    changed te naming for the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  has_many :bookings
-  has_many :bookings, through: :vehicle
+  has_many :bookings, dependent: :destroy
+  has_many :booked_vehicles, through: :bookings, source: :vehicle
+
+  has_many :owned_vehicles, class_name: "Vehicle", foreign_key: "owner_id", dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
