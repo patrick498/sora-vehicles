2c1ad9b47991c7aa35f9693664f99704cd4ca405
Andre Yuki Ide <andreyukiide@gmail.com>
2025-02-17 20:11:27 +0900

    added the seeds

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,11 +1,11 @@
 class Vehicle < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  belongs_to :owner, class_name: "User", foreign_key: "user_id"
-  has_many_attached :photos
+  belongs_to :user
+  has_many_attached :images
 
   validates :vehicle_type, :brand, :model, :description, :location, :price_day, presence: true
   enum vehicle_type: { airplane: 'airplane', car: 'car', boat: 'boat', motorcycle: 'motorcycle' }
   validates :vehicle_type, inclusion: { in: vehicle_types.keys }
-  validates :description, length: { in: 15..150 }
+  validates :description, length: { in: 15..200 }
 
 end
