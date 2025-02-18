c0e764a2259ce39810cea1afdc985548a5ec00d5
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-02-18 18:06:59 +0900

    wrote the bookings#create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,4 +7,25 @@ class BookingsController < ApplicationController
     @denied = Booking.where(user: current_user, status: "denied")
     @done = Booking.where(user: current_user, status: "done")
   end
+
+  def create
+    @vehicle = Vehicle.find(params[:vehicle_id])
+    @booking = Booking.new(booking_params)
+    total_price = (@booking.end_date - @booking.start_date) * @vehicle.price_day
+    @booking.total_price = total_price
+    @booking.vehicle = @vehicle
+    @booking.user = current_user
+    @booking.status = 'pending'
+     if @booking.save
+      redirect_to bookings_path(@vehicle)
+    else
+      render 'vehicles/show', vehicle: @vehicle, booking: @booking, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:vehicle, :user, :start_date, :end_date, :status, :total_price)
+  end
 end
