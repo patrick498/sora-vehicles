d5884f3ce2780ba2b9440c44c04c3a5a661de758
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-02-20 13:47:37 +0900

    msg

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,5 +7,12 @@ class Booking < ApplicationRecord
   validates :start_date, comparison: { greater_than: Date.today - 1 }
   enum status: { pending: 'pending', accepted: 'accepted', denied: 'denied', cancelled: 'cancelled', done: 'done' }
   validates :status, inclusion: { in: Booking.statuses.keys }
+  validate :no_overlapping_bookings
 
+  def no_overlapping_bookings
+    overlapping_bookings = Booking.where(vehicle: vehicle).where('start_date < ? AND end_date > ?', end_date, start_date)
+    if overlapping_bookings.exists?
+      errors.add(:base, "These dates are already booked. Please select different dates.")
+    end
+  end
 end
