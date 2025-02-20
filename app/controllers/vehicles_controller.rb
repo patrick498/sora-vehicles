26287708461025682311771569ac08eb1196744f
Axel Bailleres <axxelb14@gmail.com>
2025-02-19 22:50:38 -0600

    added a form to add a new car as an owner

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -43,6 +43,24 @@ class VehiclesController < ApplicationController
     @booking = Booking.new
   end
 
+  def new
+    @vehicle = Vehicle.new
+  end
 
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
+    if @vehicle.save
+      redirect_to vehicle_path(@vehicle)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+private
+
+def vehicle_params
+  params.require(:vehicle).permit(:brand, :vehicle_type, :model, :description, :location, :price_day, images:[])
+end
 
 end
